ddaf1369a972ca933e591292aa2d2d136721c221
kanikagarg-srijan <kanika.garg@srijan.net>
2020-04-21 18:03:30 +0530

    test folder

--- src/main/java/com/qa/base/WaitStatements.java
+++ src/main/java/com/qa/base/WaitStatements.java
@@ -1,5 +1,36 @@
 package com.qa.base;
 
-public class WaitStatements {
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class WaitStatements {
+public static WebDriver driver;
+	
+	public static void implicit(int Time) {
+		
+		driver.manage().timeouts().implicitlyWait(Time, TimeUnit.SECONDS);
+		
+	}
+	
+	public static void explicit_visible(WebDriver driver, WebElement element, int timeout, String value)
+	{
+		new WebDriverWait(driver, timeout).until(ExpectedConditions.visibilityOf(element));
+		element.sendKeys(value);
+		
+	}
+		
+	
+	public static void explicit_clickable(WebDriver driver, WebElement element, int timeout)
+	{
+		new WebDriverWait(driver, timeout).until(ExpectedConditions.elementToBeClickable(element));
+		element.click();
+		
+	}
+		
+	
 }
+
